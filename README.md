549aef34d00b1e3eaad653acf03de09a15f684d0
Vinicius Jabes <vinijabes@gmail.com>
2017-12-08 18:59:25 -0200

    Android
    Mudança de projeto para android

--- README.md
+++
@@ -1,1 +0,0 @@
-# hachathon-ejcomp
--- application/views/footer.php
+++
@@ -1,4 +0,0 @@
-<div id="footer" class="homebody alinhado-centro">
-<p style="font-size: 12px;">	&copy; Desenvolvido por EJComp<br>Todos os direitos reservados </p>
-</div>
-</div>
